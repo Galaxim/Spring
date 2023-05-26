d411aeb301eae66fb29c398c77c8d71279afdebf
Kristian <asdf>
2023-05-26 18:29:12 +0200

    exercise 4

--- initial/src/main/java/com/example/springboot/MealController.java
+++ initial/src/main/java/com/example/springboot/MealController.java
@@ -2,28 +2,32 @@ package com.example.springboot;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
+import org.springframework.web.bind.annotation.RequestParam;
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class MealController {
 
-    Meal pizza = new Meal("Pizza", 6.50,"good");
-    Meal superpizza = new Meal("Superpizza", 12.00,"supergood");
-    Meal hotDog = new Meal("Hot Dog", 4.80,"meh");
+    List<Meal> meals = new ArrayList<>();
 
-    @GetMapping("/meal/description-match/{phrase}")
-    public Meal getMealByDescription(@PathVariable("phrase") String description) {
-        if (description.equalsIgnoreCase("good")) {
-            return pizza;
-        } else if (description.equalsIgnoreCase("supergood")) {
-            return superpizza;
-        } else if (description.equalsIgnoreCase("meh")) {
-            return hotDog;
+    public MealController() {
+        meals.add(new Meal("Pizza", 6.50,"good"));
+        meals.add(new Meal("Superpizza", 12.00,"supergood"));
+        meals.add(new Meal("Hot Dog", 4.80,"meh"));
+    }
+
+    @GetMapping("/meal/price")
+    public List<Meal> getMealsByPriceRange(@RequestParam("min") double minPrice, @RequestParam("max") double maxPrice) {
+        List<Meal> filteredMeals = new ArrayList<>();
+
+        for (Meal meal : meals) {
+            if (meal.getPrice() >= minPrice && meal.getPrice() <= maxPrice) {
+                filteredMeals.add(meal);
+            }
         }
 
-        return null;
+        return filteredMeals;
     }
 }
-
