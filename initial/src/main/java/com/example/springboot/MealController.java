755875c44f5a7843cd3894a0929e329a00c5e066
Kristian <asdf>
2023-05-31 17:43:51 +0200

    exercise 1(Request body, Post, autoserialization issues)

--- initial/src/main/java/com/example/springboot/MealController.java
+++ initial/src/main/java/com/example/springboot/MealController.java
@@ -39,5 +39,18 @@ public class MealController {
         mealService.deleteMeal(mealName);
         return ResponseEntity.ok("Meal deleted!");
     }
+    List<Meal> meals = new ArrayList<>();
+
+    public MealController() {
+        meals.add(new Meal("Pizza", 6.50,"good"));
+        meals.add(new Meal("Superpizza", 12.00,"supergood"));
+        meals.add(new Meal("Hot Dog", 4.80,"meh"));
+    }
+
+    @PostMapping("/meal")
+    public ResponseEntity<String> addMeal(@RequestBody Meal newMeal) {
+        meals.add(newMeal);
+        return ResponseEntity.ok("Meal added successfully");
+    }
 }
 
