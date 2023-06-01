29bbd2a553dc37f9dff3e8a09d019082b2e094f5
Kristian <asdf>
2023-06-01 16:24:53 +0200

    exercise 4(Controller path variables, request params, returning objects)

--- initial/src/main/java/com/example/springboot/OtherMealController.java
+++ initial/src/main/java/com/example/springboot/OtherMealController.java
@@ -2,6 +2,7 @@ package com.example.springboot;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -34,5 +35,17 @@ public class OtherMealController {
                 return meal;
             }
         } throw new MealNotFoundException("Meal not found with this description: " + description);
+    }
+    @GetMapping("/meal/price")
+    public List<Meal> getMealsByPriceRange(@RequestParam("min") double minPrice, @RequestParam("max") double maxPrice) {
+        List<Meal> filteredMeals = new ArrayList<>();
+
+        for (Meal meal : meals) {
+            if (meal.getPrice() >= minPrice && meal.getPrice() <= maxPrice) {
+                filteredMeals.add(meal);
+            }
+        }
+
+        return filteredMeals;
     }
 }
